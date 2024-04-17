ef271e402ad823a1c5cd53f16e8dbe7ce4d5572d
DuLoS-M <e_craud@hotmail.com>
2024-04-16 18:08:14 -0600

    Added routes

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./layout/NavBar";
+import UnitConversion from "./pages/UnitConversion";
+import SumOfVectors from "./pages/SumOfVectors";
+import ProjectileTrajectory from "./pages/ProjectileTrajectory";
 
 export const router = createBrowserRouter([
     {
@@ -13,21 +16,22 @@ export const router = createBrowserRouter([
                 path: "/",
                 element: (
                     <div>
-                        About
                         <Link to="/contact">Contact</Link>
                         <Link to="/">Home</Link>
                     </div>
                 ),
             },
             {
-                path: "/contact",
-                element: (
-                    <div>
-                        Contact
-                        <Link to="/about">About</Link>
-                        <Link to="/">Home</Link>
-                    </div>
-                ),
+                path: "/unit-conversion",
+                element: <UnitConversion />,
+            },
+            {
+                path: "/vector-sum",
+                element: <SumOfVectors />,
+            },
+            {
+                path: "/projectile-movement",
+                element: <ProjectileTrajectory />,
             },
         ],
     },
