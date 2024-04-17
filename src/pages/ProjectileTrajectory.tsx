b25db5d5d6f93205a294e56305f99ac65a64026e
DuLoS-M <e_craud@hotmail.com>
2024-04-16 18:08:52 -0600

    Created pages

---
+++ src/pages/ProjectileTrajectory.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProjectileTrajectory() {
+    return <div>ProjectileTrajectory</div>;
+}
---
+++ src/pages/UnitConversion.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { convertLength } from "../utils/conversion";
+import { convertMass } from "../utils/conversion";
+import { convertTime } from "../utils/conversion";
+
+export default function UnitConversion() {
+    return <div>UnitConversion</div>;
+}
