853d42e40182fb54920a6da6716dfecfb9679624
DuLoS-M <e_craud@hotmail.com>
2024-05-21 20:11:33 -0600

    Added select to the ProjectileTrajectory page

--- src/pages/ProjectileTrajectory.tsx
+++ src/pages/ProjectileTrajectory.tsx
@@ -1,20 +1,48 @@
-import React from "react";
-import { Typography } from "antd";
+import React, { useState } from "react";
+import { Select, Typography } from "antd";
+
 /*
 Variables to find:
+Initial Height ###
 Height (y)
 H-max
 Distance (x)
 D-max?
-Velocity (initial, and final)
+Flight Time
+Initial Velocity
+Final Velocity
 Angle of the velocity
 */
 const { Title } = Typography;
 
 export default function ProjectileTrajectory() {
+    const [variable, setVariable] = useState<string>("height");
     return (
         <div className="flex flex-col items-center">
             <Title>Projectile Trajectory</Title>
+            <Select
+                placeholder="Select variable to calculate"
+                className="min-w-56"
+                value={variable}
+                onChange={(value) => setVariable(value)}
+            >
+                <Select.Option value="height">Height (y)</Select.Option>
+                <Select.Option value="h-max">H-max</Select.Option>
+                <Select.Option value="distance">Distance (x)</Select.Option>
+                {/* <Select.Option value="d-max">D-max</Select.Option> */}
+                {/* <Select.Option value="velocity">Velocity</Select.Option> */}
+                <Select.Option value="flight-time">Flight time</Select.Option>
+                <Select.Option value="initial-velocity">
+                    Initial velocity
+                </Select.Option>
+                <Select.Option value="final-velocity">
+                    Final velocity
+                </Select.Option>
+                <Select.Option value="angle">
+                    Angle of the velocity
+                </Select.Option>
+            </Select>
+            
         </div>
     );
 }
